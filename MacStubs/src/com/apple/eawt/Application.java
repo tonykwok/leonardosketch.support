bd8551a4cbf6e8d8b2ed02acaf9a02278d1c4dc7
Josh Marinacci <joshua@marinacci.org>
2010-10-26 10:19:31 -0700

    added macstubs

---
+++ MacStubs/src/com/apple/eawt/Application.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.apple.eawt;
+
+/**
+ *
+ * @author joshmarinacci
+ */
+public class Application {
+    public static Application getApplication() {
+        return null;
+    }
+
+    public void setEnabledAboutMenu(boolean b) {
+        
+    }
+
+    public void setEnabledPreferencesMenu(boolean b) {
+
+    }
+    public void addApplicationListener(ApplicationListener l) {
+        
+    }
+}
---
+++ MacStubs/src/com/apple/eawt/ApplicationListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.apple.eawt;
+
+/**
+ *
+ * @author joshmarinacci
+ */
+public interface ApplicationListener {
+     public void handleAbout(ApplicationEvent applicationEvent);
+     public void handleOpenApplication(ApplicationEvent applicationEvent);
+
+
+}
